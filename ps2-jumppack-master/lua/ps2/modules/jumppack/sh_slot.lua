2efe2e496cd391578039ad20eb79a7e54a74d5cd
Fasteroid <fasteroid@hotmail.com>
2022-05-14 19:23:12 -0400

    extract out one level

--- ps2-jumppack-master/lua/ps2/modules/jumppack/sh_slot.lua
+++
@@ -1,4 +0,0 @@
-Pointshop2.AddEquipmentSlot( "JumpPack", function( item )
-    --Check if the item is a jumpack item
-    return instanceOf( Pointshop2.GetItemClassByName( "jumppack" ), item )
-end )
